61b267283eb3560eb10373ed9f9d64c6fa52d842
Alexander Craddock <acraddoc@umd.edu>
2016-05-16 17:50:17 -0400

    Minor modifications to absGaussFit class
    Cleaning up some of the functions. Added a magnification property. Added
    some additional functions that were needed

--- absGaussFit.m
+++ absGaussFit.m
@@ -5,6 +5,7 @@ classdef absGaussFit < basicFittingClass
         %Perhaps should find some other way to set pixel size that is more
         %appropriate
         pixSize=3.69;
+        magnification=1.0;
         centreX=0;
         centreY=0;
         xCoffs=[0,1,900,300];
@@ -58,34 +59,55 @@ classdef absGaussFit < basicFittingClass
             yPix = transpose([1:length(yVec)]);
             self.yCoffs = lsqcurvefit(self.gauss,self.yCoffs,yPix,yVec,[],[],self.opts);
         end
+        %Run both fits, included for brevity in other locations
+        function runFits(self)
+            self.runXFit();
+            self.runYFit();
+        end
         %Plot the x directional slice of the cloud with its fit
         function plotX(self)
             processedImage = self.getProcessedImage();
             xVec = sum(processedImage(:,self.centreY-10:self.centreY+10),2)/21;
-            spatialVec = self.pixSize * [1:length(xVec)]- self.pixSize * self.centreX;
+            spatialVec = self.pixSize * self.magnification * [1:length(xVec)]- self.pixSize * self.magnification * self.centreX;
             plot(spatialVec,xVec,'.')
             hold all
             plot(spatialVec,self.gauss(self.xCoffs,[1:length(xVec)]))
             hold off
             xlabel('Distance in X direction (\mum)')
             ylabel('OD')
+            legend('x-data',sprintf('\\sigma_x = %.0f\\mum',self.xCoffs(4)*self.pixSize * self.magnification),'Location','southwest');
+            legend('boxoff');
         end
         %Plot the y directional slice of the cloud with its fit
         function plotY(self)
             processedImage = self.getProcessedImage();
             yVec = sum(processedImage(self.centreX-10:self.centreX+10,:),1)/21;
-            spatialVec = self.pixSize * [1:length(yVec)]- self.pixSize * self.centreY;
+            spatialVec = self.pixSize * self.magnification * [1:length(yVec)]- self.pixSize * self.magnification * self.centreY;
             plot(spatialVec,yVec,'.')
             hold all
             plot(spatialVec,self.gauss(self.yCoffs,[1:length(yVec)]))
             hold off
             xlabel('Distance in Y direction (\mum)')
             ylabel('OD')
+            legend('y-data',sprintf('\\sigma_y = %.0f\\mum',self.yCoffs(4)*self.pixSize * self.magnification),'Location','southwest');
+            legend('boxoff');
         end
         %Return centre coordinates
-        function [centreX, centreY] = getCentreCoordinates(self)
-            centreX = self.centreX;
-            centreY = self.centreY;
+        function centreCoords = getCentreCoordinates(self)
+            centreCoords.('centreX_pix') = self.centreX;
+            centreCoords.('centreY_pix') = self.centreY;
+        end
+        %Output fit values (specifically the x & y sigma atm) to a
+        %structure
+        function fitVars = getFitVars(self)
+            %note sigmaX & sigmaY are in micrometres
+            fitVars.('sigmaX_um') = self.xCoffs(4)*self.pixSize*self.magnification;
+            fitVars.('sigmaY_um') = self.yCoffs(4)*self.pixSize*self.magnification;
+        end
+        %Function to set the magnification of the lens system used to
+        %produce the shot
+        function setMagnification(self,mag)
+            self.magnification = mag;
         end
     end
     
