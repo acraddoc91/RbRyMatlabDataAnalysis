172686e1ec62defb06d381aaac174501d569ecf8
Alexander Craddock <acraddoc@umd.edu>
2016-06-28 19:20:01 -0400

    Modified refitter to work with cut data

--- refitter.m
+++ refitter.m
@@ -196,7 +196,7 @@ function saveVals_Callback(hObject, eventdata, handles)
 %function to load shot for refitting    
 function loadFile(hObject,handles)
     %import shotData to grab filename
-    shotData = evalin('base','shotData');
+    shotData = cutDataFunc;
     set(handles.indexDisp,'String',int2str(shotData(handles.indexNum).Index));
     fullFilename = char(shotData(handles.indexNum).filePath);
     set(handles.filepathDisp,'String',fullFilename);
