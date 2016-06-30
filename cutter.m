a21dc93e0db2a473636beb6a5e353fcc356174e4
Alexander Craddock <acraddoc@umd.edu>
2016-06-30 13:39:44 -0400

    Modifying cutter
    I've modified the way the cutTable functions so that it is now a table
    with three elements per row: a cut variable, a cut type and a cut value.
    This allows us to have a drop down box for the cut type in the cutter
    GUI which will prevent people screwing up the relational operators and
    breaking the cutTable

--- cutter.m
+++ cutter.m
@@ -51,7 +51,7 @@ function cutter_OpeningFcn(hObject, eventdata, handles, varargin)
     %set the selectable fields of the cutting table to be the field list
     dummyFieldList = varargin(1);
     handles.fieldList = dummyFieldList{1};
-    set(handles.cutTable,'ColumnFormat',{transpose(handles.fieldList),'char','logical'});
+    set(handles.cutTable,'ColumnFormat',{transpose(handles.fieldList),{'>','>=','<','<=','==','~='},'char','logical'});
     %check to see if a cutTable exists on the base workspace
     try
         currCutTable = evalin('base','cutTable');
@@ -61,10 +61,10 @@ function cutter_OpeningFcn(hObject, eventdata, handles, varargin)
     %load current cuts to the table
     [numCuts,~] = size(currCutTable);
     if numCuts == 0
-        set(handles.cutTable,'Data',{'','',false})
+        set(handles.cutTable,'Data',{'','','',false})
     else
         for i=1:numCuts
-            currCutTable{i,3} = true;
+            currCutTable{i,4} = true;
         end
         set(handles.cutTable,'Data',currCutTable);
     end
@@ -86,7 +86,7 @@ function varargout = cutter_OutputFcn(hObject, eventdata, handles)
 function addCut_Callback(hObject, eventdata, handles)
     %when add cut is pressed add an extra line to the cutting table
     currTableData=get(handles.cutTable,'Data');
-    set(handles.cutTable,'Data',[currTableData;{'','',false}])
+    set(handles.cutTable,'Data',[currTableData;{'','','',false}])
 
 % --- Executes during object creation, after setting all properties.
 function cutTable_CreateFcn(hObject, eventdata, handles)
@@ -106,9 +106,9 @@ function doCuts_Callback(hObject, eventdata, handles)
     cutTableOut = {};
     %strip out any cuts which aren't supposed to be made
     for i=1:numCuts
-        dummyDoCut = cutTableData(i,3);
+        dummyDoCut = cutTableData(i,4);
         if dummyDoCut{1}
-            cutTableOut = [cutTableOut;cutTableData(i,1:2)];
+            cutTableOut = [cutTableOut;cutTableData(i,1:3)];
         end
     end
     %replace base workspace cutTable
@@ -118,5 +118,5 @@ function doCuts_Callback(hObject, eventdata, handles)
 % --- Executes on button press in clearCuts.
 function clearCuts_Callback(hObject, eventdata, handles)
     %clear cutting table and clear the base workspace cutTable
-    set(handles.cutTable,'Data',{'','',false})
+    set(handles.cutTable,'Data',{'','','',false})
     assignin('base','cutTable',{});
